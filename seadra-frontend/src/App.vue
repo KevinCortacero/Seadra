085e23200718cfd932994389e6081d05d0624bf5
Kévin Cortacero <kevin.cortacero@inserm.fr>
2022-06-08 22:41:21 +0200

    App.vue wraps AppSeadra

--- seadra-frontend/src/App.vue
+++ seadra-frontend/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <!--  -->
-    </v-main>
-  </v-app>
+  <AppSeadra/>
 </template>
 
 <script>
-import axios from 'axios'
-  export default {
-    data: () => ({ drawer: null }),
-    mounted(){
-      axios.post('http://localhost:4000/fileExplorer',{'path':'/home/david/Pictures/labelomon'}).then(response => (console.log(response.data)))
-    }
+import AppSeadra from './components/AppSeadra.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppSeadra
   }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- seadra-frontend/src/components/AppSeadra.vue
+++ seadra-frontend/src/components/AppSeadra.vue
@@ -10,7 +10,7 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Seadra</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -20,7 +20,13 @@
 </template>
 
 <script>
+import axios from 'axios'
   export default {
+    name: 'AppSeadra',
     data: () => ({ drawer: null }),
+    mounted(){
+      axios.post('http://localhost:4000/fileExplorer',{'path':'./'}).then(response => (console.log(response.data)))
+    }
   }
 </script>
+
