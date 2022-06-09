273c51c832da6a9e1017e1b3b6c0e04f0272f5e3
David Bernard <bernard.david.46@gmail.com>
2022-06-09 10:13:05 +0200

    add event between osd & fabric

--- seadra-frontend/src/components/AnnotationOpenSeadragon.vue
+++ seadra-frontend/src/components/AnnotationOpenSeadragon.vue
@@ -34,12 +34,13 @@
         computed: mapState(['viewerOSD']),
         watch: {
             // whenever question changes, this function will run
-            /*
+            
             selected_tool(newSlected, oldSelected) {
-                
-            },*/
+                console.log(newSlected,oldSelected);
+            },
             viewerOSD(newOSD){
-                newOSD.canvas.appendChild(this._labelViewerDiv);
+                if(!newOSD.canvas.contains(this._labelViewerDiv)){
+                    newOSD.canvas.appendChild(this._labelViewerDiv);
 
                     this._labelCanvas = document.createElement('canvas');
                     this._labelCanvas.setAttribute('id', 'labelCanvas');
@@ -54,10 +55,97 @@
                     fabric.Object.prototype.cornerSize = 8
                     fabric.Object.prototype.hasBorders = false;
                     this._canvas._scale = 1000;
+
+                    newOSD.addHandler('canvas-press', (e) => {
+                        this.lastCenter = newOSD.viewport.getCenter(true);
+                        this._canvas._onMouseDown(e.originalEvent);
+                        e.originalEvent.stopPropagation();
+                    }); 
+                    newOSD.addHandler('canvas-release', (e) => {
+                        this._canvas._onMouseUp(e.originalEvent)
+                    });     
+                    newOSD.addHandler('canvas-click', (e) => {
+                        e.preventDefaultAction = true;
+                    });     
+                    newOSD.addHandler('canvas-drag', (e) => {
+                        this._canvas._onMouseMove(e.originalEvent)
+                    });
+                    newOSD.addHandler('zoom', () => {
+                        this.lastZoom = newOSD.viewport.getZoom(true);
+                        this.lastCenter = newOSD.viewport.getCenter(true);   
+                    });
+                    newOSD.addHandler('viewport-change', () => {
+                        if(!this.lastZoom)
+                            this.lastZoom = newOSD.viewport.getZoom(true);
+                        if(!this.lastCenter)
+                            this.lastCenter = newOSD.viewport.getCenter(true);
+                        var currentZoom = newOSD.viewport.getZoom(true);
+                        var deltaZoom = currentZoom/this.lastZoom
+                        var currentCenter = newOSD.viewport.getCenter(true);
+                        var lastWinCenter = newOSD.viewport.pixelFromPoint(this.lastCenter);
+                        var currentWinCenter = newOSD.viewport.pixelFromPoint(currentCenter);    
+
+                        this.changePosition(lastWinCenter, currentWinCenter, deltaZoom);
+                        this._canvas.renderAll();
+                        this._canvas.setViewportTransform(this._canvas.viewportTransform);
+                        this.lastZoom = currentZoom;
+                        this.lastCenter = currentCenter;
+                    });
+                    newOSD.addHandler('canvas-scroll', () => {
+                        // temporarily useless
+                    });
+                    newOSD.addHandler('canvas-enter', () => {
+                        this.mouseOut = false;
+                        if(this.drawMode) this.showXYLine();
+                        this._canvas.renderAll();
+                    })
+                    newOSD.addHandler('canvas-exit', () => {
+                        this.mouseOut = true;
+                        if(this.drawMode) this.hiddenXYLine()
+                        this._canvas.renderAll();
+                    })
+                }
             }
         },
         methods: {
-
+            resize(){
+                this._canvas.setWidth(this.viewerOSD.canvas.clientWidth);
+                this._canvas.setHeight(this.viewerOSD.canvas.clientHeight);
+            },
+            changePosition(lastPoint, currentPoint, deltaZoom) {
+                var x,y,w,h;
+                for(var i in this.labelBoxes) {
+                    var box = this.labelBoxes[i];
+                    if(box.type == 'rect') {
+                        x = (box.shape.left - currentPoint.x) * deltaZoom + lastPoint.x;
+                        y = (box.shape.top - currentPoint.y) * deltaZoom + lastPoint.y;
+                        w = box.shape.width * deltaZoom;
+                        h = box.shape.height * deltaZoom;
+                        box.shape.set('left', x);
+                        box.shape.set('top', y);
+                        box.shape.set('width', w);
+                        box.shape.set('height', h);
+                    }
+                    else if(box.type == 'poly') {
+                        x = (box.shape.left - currentPoint.x + 1) * deltaZoom + lastPoint.x - 1;
+                        y = (box.shape.top - currentPoint.y + 1) * deltaZoom + lastPoint.y - 1;
+                        w = box.shape.width * deltaZoom;
+                        h = box.shape.height * deltaZoom;
+                        box.shape.set('left', x);
+                        box.shape.set('top', y);
+                        box.shape.set('width', w);
+                        box.shape.set('height', h);
+                        var offsetX = box.shape.pathOffset.x * deltaZoom;
+                        var offsetY = box.shape.pathOffset.y * deltaZoom;
+                        box.shape.set('pathOffset', {x: offsetX, y: offsetY});
+                        var points = box.shape.points;
+                        for(var j in points) {
+                            points[j].x = points[j].x * deltaZoom;
+                            points[j].y = points[j].y * deltaZoom;
+                        }
+                    }
+                }
+            },
         },
         mounted() {
             this._labelViewerDiv = document.createElement('div');
@@ -67,84 +155,10 @@
             this._labelViewerDiv.style.top = 0;
             this._labelViewerDiv.style.width = '100%';
             this._labelViewerDiv.style.height = '100%';
-/*
-            let myInterval = setInterval(()=>{
-                if(document.getElementById('openseadragon_canvas')!==null){
-                    clearInterval(myInterval);
-                    document.getElementById('openseadragon_canvas').appendChild(this._labelViewerDiv);
-
-                    this._labelCanvas = document.createElement('canvas');
-                    this._labelCanvas.setAttribute('id', 'labelCanvas');
-                    this._labelViewerDiv.appendChild(this._labelCanvas);
-
-                    this._canvas = new fabric.Canvas('labelCanvas')
-                    this._canvas.selection = false;
-                    this._canvas.preserveObjectStacking = true;
-                
-                    fabric.Object.prototype.objectCaching = false;
-                    fabric.Object.prototype.cornerStyle = 'circle';
-                    fabric.Object.prototype.cornerSize = 8
-                    fabric.Object.prototype.hasBorders = false;
-                    this._canvas._scale = 1000;
-
-
-        this._osdViewer.addHandler('canvas-press', (e) => {
-            this.lastCenter = this._osdViewer.viewport.getCenter(true);
-            this._canvas._onMouseDown(e.originalEvent);
-            e.originalEvent.stopPropagation();
-        }); 
-        this._osdViewer.addHandler('canvas-release', (e) => {
-            this._canvas._onMouseUp(e.originalEvent)
-        });     
-        this._osdViewer.addHandler('canvas-click', (e) => {
-            e.preventDefaultAction = true;
-        });     
-        this._osdViewer.addHandler('canvas-drag', (e) => {
-            this._canvas._onMouseMove(e.originalEvent)
-        });
-        this._osdViewer.addHandler('zoom', (e) => {
-            this.lastZoom = this._osdViewer.viewport.getZoom(true);
-            this.lastCenter = this._osdViewer.viewport.getCenter(true);   
-        });
-        this._osdViewer.addHandler('open', (e) => {
-            this.loading = false;
-            this.getLabelBoxes();
-        });
-        this._osdViewer.addHandler('viewport-change', (e) => {
-            if(!this.lastZoom)
-                this.lastZoom = this._osdViewer.viewport.getZoom(true);
-            if(!this.lastCenter)
-                this.lastCenter = this._osdViewer.viewport.getCenter(true);
-            var currentZoom = this._osdViewer.viewport.getZoom(true);
-            var deltaZoom = currentZoom/this.lastZoom
-            var currentCenter = this._osdViewer.viewport.getCenter(true);
-            var lastWinCenter = this._osdViewer.viewport.pixelFromPoint(this.lastCenter);
-            var currentWinCenter = this._osdViewer.viewport.pixelFromPoint(currentCenter);    
-
-            this.changePosition(lastWinCenter, currentWinCenter, deltaZoom);
-            this._canvas.renderAll();
-            this._canvas.setViewportTransform(this._canvas.viewportTransform);
-            this.lastZoom = currentZoom;
-            this.lastCenter = currentCenter;
-        });
-        this._osdViewer.addHandler('canvas-scroll', (e) => {
-            // temporarily useless
-        });
-        this._osdViewer.addHandler('canvas-enter', (e) => {
-            this.mouseOut = false;
-            if(this.drawMode) this.showXYLine();
-            this._canvas.renderAll();
-        })
-        this._osdViewer.addHandler('canvas-exit', (e) => {
-            this.mouseOut = true;
-            if(this.drawMode) this.hiddenXYLine()
-            this._canvas.renderAll();
-        })
-
-                } else {
-                    console.log("wait mount viewer")
-                }
-            }, 100);*/
-        }
+            window.addEventListener('resize', this.resize);
+        },
+        unmounted() {
+            window.removeEventListener('resize', this.resize);
+        },
   }
 </script>
